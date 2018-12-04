b0bcd6c7b6bf09e318576a4573e4257eaf549f6c
Alejandro Manuel Seva Arroyo <asevaa@fp.uoc.edu>
2018-12-04 20:34:22 +0100

    Front end V0.3
    Se completa la página de inicio y se comienza a implementar el registro y su validación en javascript

---
+++ js/validarForm.js
@@ -0,0 +1,13 @@
+function validarForm() {
+    var pass = document.fvalida.password.value;
+    var pass2 = document.fvalida.password2.value;
+
+    if (pass!=pass2) {
+        alert("Las contraseñas no coinciden. "+" pass= "+pass+" pass2= "+pass2);
+        document.fvalida.password.focus;
+        return false;
+    } else {
+        alert("Datos listos para registrar. "+" pass= "+pass+" pass2= "+pass2);
+        return true;
+    }
+}
