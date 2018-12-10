511cdd8c2092cf2a2971300981ed0252c054de0e
LAPTOP-NPPFVRM4\javic <jcamarenam@fp.uoc.edu>
2018-12-10 13:29:03 +0100

    añadir_producto.html y añadir_producto.php

---
+++ añadir_producto.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>FrikiZone</title>
+    <!-- CSS de Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Archivo css propio-->
+    <link href="css/estilos_añadir_producto.css" rel="stylesheet">
+    </head>
+    <body>
+       <!-- Abrimos div contenedor de bootstrap para la rejilla (grid) -->
+        <div class="container text-center">
+           <!-- Iniciamos la rejilla con el logo de la imagen corporativa -->
+            <div class="row">
+                <div class="col-12">
+                    <img src="img/frikizone4.png" class="img-rounded img-fluid float-center enter" title="Logo FrikiZone" alt="Logo FrikiZone" />
+                </div>
+            </div>
+        
+            <!-- El siguiente renglón de la rejilla será el menú de navegación -->
+            <div class="row">
+                 <div class="col-12"> 
+                    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+                    <a class="navbar-brand" href="index.html">FrikiZone</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#inicio" aria-controls="barracolapsa" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="inicio">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                      <li class="nav-item ">
+                      <a class="nav-link" href="index.html">Inicio </a>
+                      </li>
+                      <li class="nav-item">
+                      <a class="nav-link" href="categorias.html">Categorías</a>
+                      </li>
+                      <li class="nav-item">
+                      <a class="nav-link" href="#">Página 3</a>
+                      </li>
+                      <li class="nav-item">
+                      <a class="nav-link" href="administracion.html">Administración</a>
+                      </li>
+                    </ul>
+                      <form class="form-inline my-2 my-lg-0">
+                       <button type="submit" name="registro" class="btn btn-outline-warning mx-4">Registrarse</button>
+                       <button type="button" name="login" class="btn btn-outline-success" onclick="location.href='login.html'">Iniciar Sesión</button>
+                    </form>
+                      
+    </div>
+    </nav>
+  </div>
+
+                   </div>
+                  
+                       
+                       
+        <h1><br>AÑADIR PRODUCTO</h1>
+			
+		<h2><br>ADMINISTRACIÓN</h2>	
+			
+			
+		
+         <div id="bloque">
+         	
+         	
+         	<form action="#" method="post">
+         	
+				<div id="izquierda">
+
+					<table style="margin-left: 20%">
+						<tr>
+							<td><center><strong>Nombre:&nbsp</strong><input type="text" name="nombre_producto" id="nombre_producto" style=" text-align: center; margin: 0px auto;border-radius: 10px 10px 10px 10px;moz-border-radius: 10px 10px 10px 10px;webkit-border-radius:10px 10px 10px 10px;border: 1px solid #000000;"></center></td>
+						</tr>
+						<tr>
+							<td style="float: left"><center><strong>Seccion:&nbsp</strong><select name="lista" style=" text-align: center; margin: 0px auto;border-radius: 10px 10px 10px 10px;moz-border-radius: 10px 10px 10px 10px;webkit-border-radius:10px 10px 10px 10px;border: 1px solid #000000;">
+								<option value="1">Comics</option>
+								<option value="2">Juegos de mesa</option>
+								<option value="3">Merchandising</option>
+							</select></center></td>
+						</tr>
+					</table>
+
+				</div>
+
+				<div id="derecha">
+					<table>
+						<tr><td><center><strong>Imagen:&nbsp</strong><input type="file" name="imagen_producto" id="imagen_producto"></center></td></tr>
+						<tr>
+							<td style="float: left"><strong>Descripción:&nbsp</strong></td>
+							<td style="float: left"><textarea class="form-control" name="descripcion_producto" id="descripcion_producto" rows="8" cols="40"></textarea></td>
+						</tr>
+					</table>
+
+				</div>
+         	
+         		<div id="boton">
+         		<center><button type="submit" name="añadir" class="btn btn-success">Añadir producto</button></center>
+         		</div>
+         	</form>
+         	
+         </div>         
+                  
+                  
+                  
+                   
+                   
+            </div>                    
+        <script src="js/jquery-3.3.1.js"></script>
+        <script src="js/bootstrap.bundle.js"></script>
+    </body>
+</html>
