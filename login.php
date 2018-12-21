7fd4cb2044889f486d50b888f3dc66f9e33ad9f8
mamen varela <44292320+MamenVarela@users.noreply.github.com>
2018-12-21 18:59:00 +0100

    Update login.php

--- login.php
+++ login.php
@@ -65,7 +65,7 @@
                        <div id="formulario_login">
 		<center><table>
 				
-				<form action="recogidalogin.php" method="post">
+				<form action="recogidalogin2.php" method="post">
 				<tr><td><center><strong>Usuario:</strong>     <input type="text" name="usuario" id="usuario" style=" text-align: center; margin: 0px auto;border-radius: 10px 10px 10px 10px;moz-border-radius: 10px 10px 10px 10px;webkit-border-radius:10px 10px 10px 10px;border: 1px solid #000000;"></center></td></tr>
 				<tr><td><center><strong>Password:</strong>     <input type="password" name="password" id="password" style=" text-align: center; margin: 0px auto;border-radius: 10px 10px 10px 10px;moz-border-radius: 10px 10px 10px 10px;webkit-border-radius:10px 10px 10px 10px;border: 1px solid #000000;"></center></td></tr>
 				<tr><td><center> <button type="submit" style="margin-top: 10%" name="login" class="btn btn-success">Acceder</button></center></td></tr>
