87abdf4a593c14bca819105550a0989479583dd9
mamen varela <44292320+MamenVarela@users.noreply.github.com>
2018-12-21 17:05:30 +0100

    Add files via upload

--- recogidaregistro.php
+++ recogidaregistro.php
@@ -1,44 +1,88 @@
 <?php
+// variables del formulario 
+$a = $_POST['nombre_usuario']; 
+$b = $_POST['apellidos']; 
+$c = $_POST['tipo_usuario']; 
+$d = $_POST['calle'];
+$e = $_POST['numero'];
+$f = $_POST['piso'];
+$g = $_POST['puerta'];
+$h = $_POST['provincia'];
+$i = $_POST['poblacion'];
+$j = $_POST['cp'];
+$k = $_POST['telefono'];
+$l = $_POST['email'];
+$m = $_POST['usuario'];
+$n = $_POST['password'];
 
-include "codigo2.php";
 
-$codigo2 = new codigo2();
 
-$codigo2->setnombre_usuario(filter_input(INPUT_POST, 'nombre_usuario'));
-$codigo2->setapellidos(filter_input(INPUT_POST, 'apellidos'));
-$codigo2->settipo_usuario(filter_input(INPUT_POST, 'tipo_usuario'));
-$codigo2->setcalle(filter_input(INPUT_POST, 'calle'));
-$codigo2->setnumero(filter_input(INPUT_POST, 'numero'));
-$codigo2->setpiso(filter_input(INPUT_POST, 'piso'));
-$codigo2->setpuerta(filter_input(INPUT_POST, 'puerta'));
-$codigo2->setprovincia(filter_input(INPUT_POST, 'provincia'));
-$codigo2->setpoblacion(filter_input(INPUT_POST, 'poblacion'));
-$codigo2->setcp(filter_input(INPUT_POST, 'cp'));
-$codigo2->settelefono(filter_input(INPUT_POST, 'telefono'));
-$codigo2->setemail(filter_input(INPUT_POST, 'email'));
-$codigo2->setusuario(filter_input(INPUT_POST, 'usuario'));
-$codigo2->setpassword(filter_input(INPUT_POST, 'password'));
 
-$nombre_usuario = $codigo2->nombre_usuario ;
-$apellidos = $codigo2->apellidos ;
-$tipo_usuario = $codigo2->tipo_usuario ;
-$calle = $codigo2->calle ;
-$numero = $codigo2->numero ;
-$piso = $codigo2->piso ;
-$puerta = $codigo2->puerta ;
-$provincia = $codigo2->provincia ;
-$poblacion = $codigo2->poblacion ;
-$cp = $codigo2->cp ;
-$telefono = $codigo2->telefono ;
-$email = $codigo2->email ;
-$usuario = $codigo2->usuario ;
-$password = $codigo2->password ;
 
-$insertar = $codigo2->insertarusuario ($nombre_usuario,$apellidos, $telefono, $email);
-$insertar2 = $codigo2-> insertardomicilio($calle, $numero, $piso, $puerta) ;
-$insertar3 = $codigo2-> insertarpoblación($poblacion, $cp);
-$insertar4 = $codigo2->insertarprovincia($provincia);
-$insertar5 = $codigo2->insertartipo_usuario($tipo_usuario);
 
-?>
 
+// conectar la base da datos 
+$db = 'id7718276_byteraiders'; 
+$host = 'localhost'; 
+$user = 'id7718276_byteraiders_user'; 
+$pass = 'Byteraiders2018'; 
+$conn = new PDO("mysql:dbname=".$db.";host=".$host,$user, $pass); 
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// iniciar transacción 
+$conn->beginTransaction();
+
+try { 
+// tabla 1 
+$sql = "INSERT INTO Provincia (Provincia) VALUES (:Provincia);"; 
+$result = $conn->prepare($sql); 
+$result->bindValue(':Provincia', $h, PDO::PARAM_STR); 
+$result->execute(); 
+$lastId = $conn->lastInsertId();
+
+// tabla 2 
+$sql ="INSERT INTO Poblacion (Provincia,Poblacion,cp) VALUES (:h_id,:Poblacion, :cp) ;";
+$result = $conn->prepare($sql); 
+$result->bindValue(':h_id', $lastId, PDO::PARAM_INT); 
+$result->bindValue(':Poblacion', $i, PDO::PARAM_STR); 
+$result->bindValue(':cp', $j, PDO::PARAM_INT); 
+$result->execute();
+$lastId = $conn->lastInsertId();
+
+$sql ="INSERT INTO Calle (Calle,Numero,Piso, Puerta) VALUES (:Calle,:Numero, :Piso,:Puerta) ;";
+$result = $conn->prepare($sql); 
+$result->bindValue(':Calle', $d, PDO::PARAM_STR); 
+$result->bindValue(':Numero', $e, PDO::PARAM_INT); 
+$result->bindValue(':Piso', $f, PDO::PARAM_STR); 
+$result->bindValue(':Puerta', $g, PDO::PARAM_STR); 
+$result->execute();
+$lastId = $conn->lastInsertId();
+
+// tabla 3 
+$sql = "INSERT INTO tipousuarios (tipo) VALUES (:tipo) ;";
+$result = $conn->prepare($sql); 
+$result->bindValue(':tipo', $c, PDO::PARAM_STR); 
+$result->execute();
+$lastId = $conn->lastInsertId();
+
+// tabla 4 
+$sql =  "INSERT INTO usuarios (tipousuario,calle, poblacion, usuario, password, nombre, apellidos, telefono, email )VALUES (:c_id,:d_id,:i_id, :usuario, :password, :nombre,:apellidos,:telefono,:email) ;";
+$result = $conn->prepare($sql); 
+$result->bindValue(':c_id', $lastId, PDO::PARAM_INT); 
+$result->bindValue(':d_id', $lastId, PDO::PARAM_INT); 
+$result->bindValue(':i_id', $lastId, PDO::PARAM_INT);
+$result->bindValue(':usuario', $m, PDO::PARAM_STR); 
+$result->bindValue(':password', $n, PDO::PARAM_STR);
+$result->bindValue(':nombre', $a, PDO::PARAM_STR);
+$result->bindValue(':apellidos', $b, PDO::PARAM_STR);
+$result->bindValue(':telefono', $k, PDO::PARAM_STR);
+$result->bindValue(':email', $l, PDO::PARAM_STR);
+$result->execute();
+
+$conn->commit(); 
+echo 'Datos insertados'; 
+} catch (PDOException $e) { 
+// si ocurre un error hacemos rollback para anular todos los insert 
+$conn->rollback(); 
+echo $e->getMessage();; 
+} 
