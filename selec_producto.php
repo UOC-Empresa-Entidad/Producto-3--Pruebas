7ca1d3002f0e407eb6b17b2251b7daaec07fcf76
mamen varela <44292320+MamenVarela@users.noreply.github.com>
2018-12-27 15:26:27 +0100

    Update selec_producto.php

--- selec_producto.php
+++ selec_producto.php
@@ -4,12 +4,7 @@
 
 
    
-$db = 'P8'; 
-$host = 'localhost'; 
-$user = 'root'; 
-$pass = ''; 
-$conn = new PDO("mysql:dbname=".$db.";host=".$host,$user, $pass); 
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+require_once("./conexion.php");
 
 // iniciar transacción 
 $conn->beginTransaction();
